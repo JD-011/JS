979fc129464c9c1e4c7895ade89d8216fcab6181
Jayveer Dahiya <156442984+JD-011@users.noreply.github.com>
2025-01-15 06:56:01 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+let score = "11abc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+/*
+"11" => 11
+"11abc" => NaN (Not a Number)
+true => 1, false => 0
+null => 0, undefined => NaN
+*/
+
+let isLoggedIn = "jayveer"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+/*
+1 => true, 0 => false
+"" => false
+"jayveer" => true
+*/
+
+let someNumber = 11
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
