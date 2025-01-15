d88aa69ff946d7785be075f24afb20c0b342b4fd
Jayveer Dahiya <156442984+JD-011@users.noreply.github.com>
2025-01-15 09:04:11 +0000

    Comparisions of datatypes

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+/* 
+make sure that both datatypes sholud be same at the end whenever you do like this, 
+or it will give something unexpected, like below examples so try to avoid this:
+*/
+
+// console.log(null > 0);
+// console.log(null == 0); // in JS == and other comparision op(<,>...) works differently
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === : strict check
+
+console.log("2" == 2);
+console.log("2" === 2);
